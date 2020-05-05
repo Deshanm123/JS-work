38cee403c31f22519282fc48c3f5b3049930e96b
Deshanm123 <deshanm123@gmail.com>
2020-05-05 23:22:22 +0530

    add check function
    Show error and  Show success for user name

--- form validation -JS/script.js
+++ form validation -JS/script.js
@@ -3,11 +3,29 @@ const userName =document.getElementById("userName");
 const email =document.getElementById("email");
 const passsword =document.getElementById("password");
 const password2 =document.getElementById("password2");
+ //function:Show input Error msg
+ function showError(input,msg){
+     //Selecting the formcontrol div element out of inputdata
+     const formControl =input.parentElement;
+     //overiding the class of parent
+     formControl.className = "form-control error";
+     const small = formControl.querySelector('small');
+     small.innerText=msg;
+ }
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
 
+//Event Listeners
 form.addEventListener('submit',function(e) {
     e.preventDefault();//prevent form submission and take over the process
-   console.log(userName.value);
-    
+   
+    if(userName.value === ""){
+        showError(userName,"User Name is Required");
+    }else{
+        showSuccess(userName);
+    }
 
 
 
