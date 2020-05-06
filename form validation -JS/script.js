d82902065e08a90c0a14c8fc7a763871e59c9382
Deshanm123 <deshanm123@gmail.com>
2020-05-06 20:55:36 +0530

    Check Frunctions
    Repitionss occur

--- form validation -JS/script.js
+++ form validation -JS/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const userName =document.getElementById("userName");
 const email =document.getElementById("email");
-const passsword =document.getElementById("password");
+const password =document.getElementById("password");
 const password2 =document.getElementById("password2");
  //function:Show input Error msg
  function showError(input,msg){
@@ -16,7 +16,13 @@ function showSuccess(input){
     const formControl = input.parentElement;
     formControl.className = "form-control success";
 }
-
+//Check Emial is valid 
+function isEmailValid(email){
+    
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+       return re.test(String(email).toLowerCase());
+    
+}
 //Event Listeners
 form.addEventListener('submit',function(e) {
     e.preventDefault();//prevent form submission and take over the process
@@ -27,6 +33,26 @@ form.addEventListener('submit',function(e) {
         showSuccess(userName);
     }
 
+    if (email.value ===""){
+        showError(email,"Email is Required");
+    }else if(!isEmailValid(email.value)){
+        showError(email,"Email is not Valid");
+    }
+    else{
+        showSuccess(email);
+    }
+
+    if(password.value === ""){
+       showError(password,"Enter password");
+    }else{
+        showSuccess(password);
+    }
+    if(password2.value === ""){
+        showError(password2,"Enter Confirm password");
+    }else{
+        showSuccess(password2);
+    }
+    
 
 
 });
